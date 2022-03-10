b59a560d346a6a044577f42419bd451341ee216c
sunyoungyang <65889734+sunyoung-yang@users.noreply.github.com>
2022-03-10 23:16:21 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 316 ms, Memory: 18096 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+       sc.close();
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
