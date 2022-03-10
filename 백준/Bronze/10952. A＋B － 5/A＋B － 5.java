06f87c8a3c04d6fbd328033cde3a6f828ba670aa
sunyoungyang <65889734+sunyoung-yang@users.noreply.github.com>
2022-03-10 23:13:07 +0900

    [Bronze III] Title: A+B - 5, Time: 164 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int sum = 0;
+
+        while (true) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum = a + b;
+            if (a == 0 && b == 0) {
+                break;
+            }
+            bw.write(sum + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+
+}
