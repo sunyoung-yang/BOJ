fd3a1747a5533274081c334f5b6bf4f97857babf
sunyoungyang <65889734+sunyoung-yang@users.noreply.github.com>
2022-03-10 23:19:07 +0900

    [Bronze III] Title: N 찍기, Time: 1100 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        sc.close();
+
+        for (int i = 1; i <= N; i++) {
+
+            System.out.println(i);
+        }
+
+    }
+
+}
