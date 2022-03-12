2fe566add67ecd590a62d6ebcb830b80c7590016
sunyoungyang <65889734+sunyoung-yang@users.noreply.github.com>
2022-03-12 12:35:58 +0900

    [Bronze II] Title: 나머지, Time: 148 ms, Memory: 14864 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        HashSet<Integer> list = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            st = new StringTokenizer(bf.readLine());
+            list.add((Integer.parseInt(st.nextToken())) % 42);
+        }
+
+        //System.out.println(list.size());
+        bw.write(list.size() + "\n");
+        bw.flush();
+
+    }
+
+}
