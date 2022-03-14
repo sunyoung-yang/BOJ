059f04d0264fb259b492e393624b31b80b7f9e8d
sunyoungyang <65889734+sunyoung-yang@users.noreply.github.com>
2022-03-14 23:40:53 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -2,33 +2,40 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt(); // 과목의 수
-        double[] subject = new double[count]; // 과목별 점수
-        int max = 0; // 과목 최대 점수
-        double sum = 0; // 점수의 합
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        // 과목 별로 점수를 입력 받고, 최대 점수를 구함
-        for (int i = 0; i < count; i++) {
-            subject[i] = sc.nextFloat();
-            max = (int) Math.max(max, subject[i]);
+        int n = Integer.parseInt(br.readLine());
+        double[] arr = new double[n];
+        int M = 0; // max
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (M < arr[i]) {
+                M = (int) arr[i];
+            }
         }
 
-        // 점수 조작
-        for (int i = 0; i < count; i++) {
-            subject[i] = (double) subject[i] / max * 100;
-            sum += subject[i];
+        double sum = 0;
+        double result = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = (arr[i] / M) * 100;
+            result += sum;
         }
 
-        // 조작된 점수의 평균 출력
-        System.out.println(sum / count);
+        System.out.print(result / n);
+        // bw.write(double+"\n");
+        // bw.flush();
     }
 
 }
