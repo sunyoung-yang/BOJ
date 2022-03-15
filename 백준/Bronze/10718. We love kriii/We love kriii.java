78a44aeeb98d2f80c9b0aa2e3fa4083dfa5945e3
sunyoungyang <65889734+sunyoung-yang@users.noreply.github.com>
2022-03-15 16:14:04 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 13848 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+
+    }
+
+}
