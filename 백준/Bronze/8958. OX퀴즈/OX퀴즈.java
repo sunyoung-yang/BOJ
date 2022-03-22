acf73f61aa9f1d6f9821f073a8199e964f1b3cd4
sunyoungyang <65889734+sunyoung-yang@users.noreply.github.com>
2022-03-22 19:20:09 +0900

    [Bronze II] Title: OX퀴즈, Time: 140 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int A = Integer.parseInt(br.readLine());
+        String[] arr = new String[A];
+
+        for (int i = 0; i < A; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = (st.nextToken());
+
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            int cnt = 0;
+            int sum = 0;
+
+            for (int i = 0; i < arr[j].length(); i++) {
+                if (arr[j].charAt(i) == 'O') {
+                    cnt++;
+                    sum += cnt;
+                } else {
+                    cnt = 0;
+                }
+
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
